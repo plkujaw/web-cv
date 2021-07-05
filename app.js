37f188096ae5e38dcc9b891d485175e1127d0449
Kuba <jawor.jakub@gmail.com>
2021-07-05 09:07:09 +0100

    update

--- app.js
+++ app.js
@@ -1,73 +1,58 @@
 const projects = [
   {
     id: 1,
-    name: "Community Video",
-    info:
-      "Platform where users can upload videos sharing their skills. It was inspired by the CoVid19 outbreak where most of the world had to respect social distancing and have most of their lives occur in the online world. The goal was to allow community members to be able to learn new skills during the time of lockdown and support each other.",
+    name: 'The Kings Arms',
+    info: 'WordPress website for a local pub.',
     stack:
-      "Tech stack: Ruby on Rails, PostgreSQL, PayPal API, JavaScript, CSS, RSpec, Capybara.",
-    links: `<a href="https://github.com/plkujaw/co-vid20-20" target="_blank" rel="noreferrer"><img src="img/github.png" alt="github repo" class="icon"></a>
-            <a href="https://youtu.be/xNpah5xL_l0?t=2597" target="_blank" rel="noreferrer"><img src="img/yt.png" alt="demo video" class="icon-long"></a>`,
-    img: "img/cov.png",
+      'Tech stack: WordPress, Timber/Twig, ACF, SCSS, gulp.js, JavaScript, jQuery.',
+    links: `<a href="https://kingsarmsatstrete.co.uk/" target="_blank" rel="noreferrer"><img src="img/www.png" alt="website" class="icon"></a>`,
+    img: 'img/tka.png',
   },
   {
     id: 2,
-    name: "Rock, paper, scissors",
-    info:
-      "Classic rock, paper, scissors game developed following TDD principles.",
-    stack: "Tech stack: Ruby, Sinatra, CSS, RSpec, Capybara.",
-    links: `<a href="https://github.com/plkujaw/rps-challenge" target="_blank" rel="noreferrer"><img src="img/github.png" alt="rock paper scissors" class="icon"></a>`,
-    img: "img/rps.png",
+    name: 'Biotechflow',
+    info: 'WordPress build for a bio-technology company that specialises in intensified bio separations, Downstream Process using Expanded Bed Adsorption chromatography step.',
+    stack:
+      'Tech stack: WordPress, Timber/Twig, ACF, SCSS, gulp.js, JavaScript, jQuery, Lottie.',
+    links: `<a href="https://biotechflow.com/" target="_blank" rel="noreferrer"><img src="img/www.png" alt="website" class="icon"></a>`,
+    img: 'img/btf.png',
   },
   {
     id: 3,
-    name: "Colour flipper",
-    info:
-      "Background colour generator and flipper written in Vanilla JavaScript.",
-    stack: "Tech stack: Vanilla JavaScript, HTML, CSS.",
-    links: `<a href="https://github.com/plkujaw/js-colour-flipper" target="_blank" rel="noreferrer"><img src="img/github.png" alt="github repo" class="icon"></a>
-            <a href="https://plkujaw.github.io/js-colour-flipper/index.html" target="_blank" rel="noreferrer"><img src="img/www.png" alt="website" class="icon"></a>`,
-    img: "img/flipper-demo.gif",
+    name: 'Hightide',
+    info: 'WordPress website for kayak school and hire company based in Switzerland.',
+    stack:
+      'Tech stack: WordPress, Timber/Twig, ACF, SCSS, gulp.js, JavaScript, jQuery. Used Gutenberg blocks for modularity.',
+    links: `<a href="http://hightide.ch/" target="_blank" rel="noreferrer"><img src="img/www.png" alt="website" class="icon"></a>`,
+    img: 'img/ht.png',
   },
   {
     id: 4,
-    name: "Corona Run Game",
-    info: `Project built for <a href="https://twitter.com/hashtag/hackforheroes" target="_blank" rel="noreferrer">#hackforheroes</a>, a two days and a half hackathon organized by Makers. The main theme was developing an app inspired by the Covid-19 crisis.`,
-    stack: "Tech stack: JavaScript, p5.js.",
-    links: `<a href="https://github.com/plkujaw/hfh_corona_game" target="_blank" rel="noreferrer"><img src="img/github.png" alt="hack for heroes" class="icon"></a>`,
-    img: "img/hfh.png",
+    name: 'E28 BMW M5 website.',
+    info: `If the E28 BMW M5 was a brand new car on sale today...`,
+    stack: 'Tech stack: HTML, CSS, jQuery, Lottie, Bootstrap.',
+    links: `<a href="https://pistonnerd.com/" target="_blank" rel="noreferrer"><img src="img/www.png" alt="website" class="icon"></a>`,
+    img: 'img/bmw.png',
   },
   {
     id: 5,
-    name: "Palindrome detector",
-    info: "Check if the given phrase is a palindrome.",
-    stack: "Tech stack: Vanilla JavaScript, Mocha, HTML, CSS.",
+    name: 'Tumu Consulting',
+    info: 'WordPress build for a civil engineering consultancy.',
+    stack:
+      'Tech stack: WordPress, Timber/Twig, ACF, SCSS, gulp.js, JavaScript, jQuery',
     links: `<a
-            href="https://github.com/plkujaw/js-palindrome-detector"
+            href="https://www.tumuconsulting.com/"
             target="_blank"
             rel="noreferrer"
-            ><img src="img/github.png" alt="github repo" class="icon"
-          /></a>
-          <a
-            href="https://plkujaw.github.io/js-palindrome-detector/"
-            target="_blank"
-            rel="noreferrer"
-            ><img src="img/www.png" alt="website" class="icon"
-          /></a>
-          <a
-            href="https://www.npmjs.com/package/plkujaw-palindrome"
-            target="_blank"
-            rel="noreferrer"
-            ><img src="img/npm.png" alt="npm" class="icon"
-          /></a>`,
-    img: "img/palindrome.gif",
+            ><img src="img/www.png" alt="website" class="icon"></a>
+          `,
+    img: 'img/tumu.png',
   },
   {
     id: 6,
-    name: "Big City",
-    info:
-      "Create a functioning, responsive website based only on a Photoshop design/layout.",
-    stack: "Tech stack: HTML, CSS, JavaScript, Jest.",
+    name: 'Big City',
+    info: 'Create a functioning, responsive website based only on a Photoshop design/layout.',
+    stack: 'Tech stack: HTML, CSS, JavaScript, Jest.',
     links: `<a
             href="https://github.com/plkujaw/bigcity"
             target="_blank"
@@ -80,37 +65,37 @@ const projects = [
             rel="noreferrer"
             ><img src="img/www.png" alt="website" class="icon"
           /></a>`,
-    img: "img/bigcity.png",
+    img: 'img/bigcity.png',
   },
   {
     id: 7,
-    name: "Greenheart",
-    info:
-      "Build a custom WordPress website and theme for social & environmental impact consultancy",
-    stack: "Tech stack: WordPress, Timber/Twig, Advanced Custom Fields, HTML, SCSS, JavaScript, Gulp.js.",
+    name: 'Greenheart',
+    info: 'Build a custom WordPress website and theme for social & environmental impact consultancy',
+    stack:
+      'Tech stack: WordPress, Timber/Twig, Advanced Custom Fields, HTML, SCSS, JavaScript, Gulp.js.',
     links: `<a
             href="https://greenheartbusiness.com/"
             target="_blank"
             rel="noreferrer"
             ><img src="img/www.png" alt="website" class="icon"
           /></a>`,
-    img: "img/greenheart.jpg",
+    img: 'img/greenheart.jpg',
   },
 ];
 
-const img = document.getElementById("project-img");
-const name = document.getElementById("name");
-const info = document.getElementById("info");
-const stack = document.getElementById("stack");
-const links = document.getElementById("links");
+const img = document.getElementById('project-img');
+const name = document.getElementById('name');
+const info = document.getElementById('info');
+const stack = document.getElementById('stack');
+const links = document.getElementById('links');
 
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const randomBtn = document.querySelector(".random-btn");
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
 let currentItem = projects.length - 1;
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   showProject(currentItem);
 });
 
@@ -123,7 +108,7 @@ showProject = (project) => {
   links.innerHTML = item.links;
 };
 
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener('click', () => {
   currentItem++;
   // when we reach last element in the array we start from the beginning
   if (currentItem > projects.length - 1) {
@@ -135,7 +120,7 @@ nextBtn.addEventListener("click", () => {
 
 // show previous review
 
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener('click', () => {
   currentItem--;
   // when we reach last element in the array we start from the last item
   if (currentItem < 0) {
@@ -147,8 +132,8 @@ prevBtn.addEventListener("click", () => {
 
 // show random review
 
-randomBtn.addEventListener("click", () => {
+randomBtn.addEventListener('click', () => {
   currentItem = Math.floor(Math.random() * projects.length);
   showProject(currentItem);
-  window.location.replace("index.html#projects");
+  window.location.replace('index.html#projects');
 });
